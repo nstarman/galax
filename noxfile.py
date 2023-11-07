07b0a3ccca86f911c68dc5516d42eccc7e643e7c
nstarman <nstarman@users.noreply.github.com>
2023-11-07 13:29:11 -0500

    nox tests
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- noxfile.py
+++ noxfile.py
@@ -22,15 +22,15 @@ def lint(session: nox.Session) -> None:
     )
 
 
-@nox.session
-def pylint(session: nox.Session) -> None:
-    """
-    Run PyLint.
-    """
-    # This needs to be installed into the package environment, and is slower
-    # than a pre-commit check
-    session.install(".", "pylint")
-    session.run("pylint", "galdynamix", *session.posargs)
+# @nox.session
+# def pylint(session: nox.Session) -> None:
+#     """
+#     Run PyLint.
+#     """
+#     # This needs to be installed into the package environment, and is slower
+#     # than a pre-commit check
+#     session.install(".", "pylint")
+#     session.run("pylint", "galdynamix", *session.posargs)
 
 
 @nox.session
